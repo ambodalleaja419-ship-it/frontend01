056ddb60d5c89625a697d9afe42fd299d66da0f0
ambodalleaja419 <ambodalleaja419@gmail.com>
2026-03-25 21:28:46 -0700

    updatecode

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -33,7 +33,7 @@ export default function AuthForm() {
   const [notifications, setNotifications] = useState<{id: number, text: string}[]>([]);
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
-  const API_URL = "https://web-production-f31a0.up.railway.app";
+  const API_URL = "https://web-production-f31a0.up.railway.app/register";
 
   // Efek Notifikasi Berjejer dari Bawah
   useEffect(() => {
